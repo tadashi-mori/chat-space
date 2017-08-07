38a840410f75bff9781074b1d8b22a92d2751238
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-07 14:09:30 +0900

    add view

--- db/migrate/20170807045750_create_groups.rb
+++ db/migrate/20170807045750_create_groups.rb
@@ -1,9 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-      t.text :name, null:false
-      add_index :groups, :name, unique: true
-
+      t.text :name, null:false, unique: true
       t.timestamps
     end
   end
