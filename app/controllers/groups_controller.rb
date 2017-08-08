00cd1122661057c34f47da171b4cb5a3a363757a
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-08 18:26:17 +0900

    edit group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < ApplicationController
 
+  def index
+    @group = Group.all
+  end
+
   def new
     @group = Group.new
   end
@@ -14,14 +18,21 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find_by(params[:id])
   end
 
   def update
+    @group = Group.find_by(params[:id])
+    if @group.save
+      redirect_to :root, notice: "グループを編集しました。"
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
-    params.require(:group).permit(:name,{users_ids: []})
+    params.require(:group).permit(:name,{user_ids: []})
   end
 
 end
