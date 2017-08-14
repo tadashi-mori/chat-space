95f61569a3d5f589feafbcda9b65161c6945bd81
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-14 17:04:56 +0900

    add gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'groups#index'
   devise_for :users
+  root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only:[:index, :new, :create, :edit, :update,] do
     resources :messages, only:[:create, :index]
