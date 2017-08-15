289f27207749e35487ab15288b01e062df590a61
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-16 08:01:59 +0900

    add test if don't login root

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,23 +1,34 @@
-require 'rails_helper'
+  require 'rails_helper'
 
-RSpec.describe MessagesController, type: :controller do
-  let(:user) {create(:user)}
-  let(:group) {create(:group)}
+  RSpec.describe MessagesController, type: :controller do
+    let(:user) {create(:user)}
+    let(:group) {create(:group)}
+    # let(:groups) {current_user.groups}
 
-  describe 'GET #index' do
-    before do
-      login_user user
+    describe 'GET #index' do
+      context "ログインしている場合" do
+        before do
+          login_user user
+        end
+
+        it "アクション内で定義しているインスタンス変数はあるか" do
+          get :index, params: { group_id: group }
+          expect(assigns(:group)).to eq group
+        end
+
+        it "該当するビューが描写されているか" do
+          get :index, params: { group_id: group }
+          expect(response).to render_template :index
+        end
+      end
+
+      context "ログインしていない場合" do
+
+        it "意図した画面にリダイレクトできているか" do
+          get :index, params: { group_id: group }
+          expect(response).to redirect_to new_user_session_path
+
+        end
+      end
     end
-
-    it "アクション内で定義しているインスタンス変数はあるか" do
-      get :index, params: { group_id: group }
-      expect(assigns(:group)).to eq group
-    end
-
-    it "該当するビューが描写されているか" do
-      get :index, params: { group_id: group }
-      expect(response).to render_template :index
-    end
-
-  end
 end
