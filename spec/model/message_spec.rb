4e6a0b10531f5aa4d314bbef89c2d7afb021b0ee
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-15 14:28:02 +0900

    add rspec if message save

--- spec/model/message_spec.rb
+++ spec/model/message_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 describe Message do
   describe '#create'do
-  it "is valid with a message" do
+  it "is valid with a body" do
     message = build(:message, image:"")
+    message.valid?
+    expect(message).to be_valid
+  end
+
+  it "is valid with a image" do
+    message = build(:message, body:"")
+    message.valid?
+    expect(message).to be_valid
+  end
+
+  it "is valid with a body and image" do
+    message = build(:message)
     message.valid?
     expect(message).to be_valid
   end
