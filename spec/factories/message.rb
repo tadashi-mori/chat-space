1162386d0bf51cde8d398ef3c7c877ceeae481b7
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-15 14:56:10 +0900

    fix factories/message.rb
    アソシエーションを定義しているため、必要ない記述を削除しました。

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
   factory :message do
     body          "nekoneko"
     image         Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/images.jpg'))
-    group_id      "1"
-    user_id       "1"
     created_at    {Faker::Time.between(2.days.ago, Date.today, :all)}
     updated_at    {Faker::Time.between(2.days.ago, Date.today, :all)}
 
