91609c76157ab4b6f3bf124d6ee945b4823f735a
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-16 14:19:49 +0900

    re fix space

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   pass = Faker::Internet.password(8)
 
   factory :user do
-    name                  {Faker::Name.name}
-    email                 {Faker::Internet.email}
+    name                  { Faker::Name.name }
+    email                 { Faker::Internet.email }
     password              pass
     password_confirmation pass
   end
