569834cbfa7105b5418bd77e31feaf2f32a65561
tadashi-mori <tadashi.mori.1110@gmail.com>
2017-08-21 00:58:29 +0900

    re fix user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,45 +1,44 @@
 $(function() {
 
-  var users_list = $('#user-search-result');
+  var search_list = $('#user-search-result');
+  var user_list = $('.chat-group-users');
 
 
 // #インクリメンタルサーチのhtmlを定義
 function searchUser(user) {
   var html = `<div class="chat-group-user clearfix">
                <p class="chat-group-user__name">${user.name}</p>
-               <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
-              </div>`
+               <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+              </div>`;
 
-  users_list.append(html);
+  search_list.append(html);
 }
 
 // #一致するユーザーはいませんの表示
 function message(message){
   var html = `<div class="chat-group-user clearfix">
                <p class="chat-group-user__name">${message}</p>
-              </div>`
+              </div>`;
 
   users_list.append(html);
 }
 
 // ＃追加するメンバーの表示
-function addUser(user_id, user_name){
-  var add_user    =$('#user_add_result');
-  var delete_user =$('#user-search-result');
-  var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-${user_id}">
-                <input name="group[user_ids][]" type="hidden" value="${user_id}">
-                <p class="chat-group-user__name">${user_name}</p>
-                <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
-
-  add_user.append(html);
-  delete_user.remove();
-}
-
-// ＃メンバーの削除
- function removeUser(){
-  var delete_member = $('#user_add_result')
-  delete_member.remove();
- }
+function addUser(user_id, user_name) {
+    var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">`
+        html += user_name
+        html += `</p>
+                <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="`
+        html += user_id
+        html += `" data-user-name="`
+        html += user_name
+        html +=`">
+                削除
+                </a>
+                </div>`
+    user_list.append(html);
+  }
 
 
 
@@ -73,11 +72,16 @@ $("#user-search-field").on("keyup", function() {
 
 
 // ＃メンバーの追加機能
+ $(document).on('click', '.chat-group-user__btn--add',function(){
+   var id = $(this).attr('data-user-id');
+   var name =$(this).attr('data-user-name');
   addUser(id, name);
 });
 
+
+
 // ＃メンバーの削除
- $(document).on("click",".js-remove-btn",function(){
-  removeUser();
+   $(document).on('click', '.chat-group-user__btn--remove', function(){
+    $(this).parent('.chat-group-user').remove();
  })
 });
